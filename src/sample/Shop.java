5dec0f7bb0e5b2cf020e9cb49ec78ba1853dbfd0
Daria <rageiscoming@gmail.com>
2019-12-22 13:01:54 +0300

    fixes

---
+++ src/sample/Shop.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Shop {
+    private Admin аAdmin;
+    private Cashbox аCashbox;
+    private Buyer аBuyer;
+    private Stock аStock;
+
+    public void goOut(){}
+    public void goIn(){}
+    public Stock getStock(){ return аStock; }
+}
